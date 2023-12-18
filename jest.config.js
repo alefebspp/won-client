bb0885857acc8aa161e489eb4377dc61dd15b2a9
Alefe Bispo <alefe.bispo@onesight.com.br>
2023-12-18 20:24:19 -0300

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/app/**', // should be tested in e2e
+    '!src/lib/registry.tsx',
+    '!src/**/stories.tsx',
+    '!src/types/**',
+    '!src/styles/**',
+    '!src/utils/**'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  },
+  moduleNameMapper: {
+    '^styled-components':
+      'styled-components/dist/styled-components.browser.cjs.js',
+      '^@/utils/(.*)$': '<rootDir>/src/utils/$1'
+  }
+}
